4e8f26e3af51c60a57f0f7001c633959b976e335
Neil Harlow <neil@y-designs.com>
2018-07-03 15:44:59 -0700

    more work on the teaser listing view, removed the onscroll transition tied to the teaser video

--- src/modules/video/components/TeaserCard.js
+++ src/modules/video/components/TeaserCard.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { findDOMNode } from 'react-dom';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
+import IconButton from '@material-ui/core/IconButton';
+import PlayIcon from '@material-ui/icons/PlayArrow';
 import '../styles/teaser-card.css';
 
 
@@ -10,50 +12,27 @@ type Props = {
 }
 
 export default class TeaserCard extends Component<Props> {
-    constructor() {
-        super()
-        this.state = {
-            mediaContainerWidth: 100
-        }
-    }
-    componentDidMount() {
-        if (window.addEventListener) {
-            findDOMNode(this._contentContainerRef).addEventListener('scroll', this._handleContentScroll.bind(this), false)
-        } else {
-            findDOMNode(this._contentContainerRef).attachEvent('onscroll', this._handleContentScroll.bind(this), false)
-        }
-    }
-    componentWillUnmount() {
-        if (window.addEventListener) {
-            findDOMNode(this._contentContainerRef).removeEventListener('scroll', this._handleContentScroll.bind(this), false)
-        } else {
-            findDOMNode(this._contentContainerRef).attachEvent('onscroll', this._handleContentScroll.bind(this), false)
-        }
-    }
-    _handleContentScroll = (event) => {
-        const widthPercentage = Math.max(100 - event.target.scrollTop * 50 / 250, 50)
-        this.setState({
-            mediaContainerStyle: {
-                width: `${widthPercentage}%`
-            }
-        })
-    }
     render() {
         const { video } = this.props
         return (
             <div className="TeaserCard">
-                <div className="media-container" style={this.state.mediaContainerStyle}>
-                    <div className="video-container" style={{backgroundImage: `url(${video.coverImageUrl})`}}></div>                                
+                <div className="media-container">
+                    <div className="video-container" style={{backgroundImage: `url(${video.coverImageUrl})`}}></div>
                     <Typography variant="title" className="title">
                         {video.title}
                     </Typography>
+                    <div className="action-pane">
+                        <Typography variant="body1">{`LIVE | 15 AO | watch for free`}</Typography>
+                        <div className="play-button-container">
+                            <IconButton>
+                                <PlayIcon />
+                            </IconButton>
+                            <Typography variant="subheading">{`Watch`}</Typography>
+                        </div>
+                    </div>
                 </div>
-                <div className="content-container" ref={ref => this._contentContainerRef = ref}>
-                    <Grid container spacing={16} style={{height: 800}}>
-                        <Grid item xs={6}>
-                            <Typography variant="body1">{`LIVE | 15 AO | watch for free`}</Typography>
-
-                        </Grid>
+                <div className="content-container">
+                    <Grid container spacing={16} alignItems="flex-start" justify="flex-end" style={{height: 800}}>
                         <Grid item xs={6}>
                             <Typography variant="body1">
                                 {video.description}
