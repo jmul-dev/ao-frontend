76fcb8c2ef8cad1e33eed8b5aabc5375aac4351a
Neil Harlow <neil@y-designs.com>
2018-07-12 12:53:41 -0700

    stake transaction placeholder (using web3 sign), also slightly sidetracked with some layout styling and navigation fix

--- src/modules/upload/components/UploadFormSubmit.js
+++ src/modules/upload/components/UploadFormSubmit.js
@@ -3,19 +3,32 @@ import React, { Component } from 'react';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import withUploadFormMutation, { UploadFormMutationProps } from '../containers/withUploadFormMutation';
+import { Redirect } from 'react-router-dom';
+
 
 type Props = {
     form: Object,
+    stakeTransaction: {
+        result: any,
+        error: any
+    },
+    triggerStakeTransaction: Function,
     ...UploadFormMutationProps
 }
 
 class UploadFormSubmit extends Component<Props> {
     props: Props;
     componentDidMount() {
-        // this.props.submitContent()
+        // TODO: check if form is valid
+        if ( this.props.form.video ) {
+            this.props.triggerStakeTransaction()
+        }
     }
     render() {
-        const { submitContentLoading, submitContentError } = this.props
+        const { submitContentLoading, submitContentError, form, stakeTransaction } = this.props
+        if ( !form.video ) {
+            return <Redirect to={'/app/view/upload/start'} />
+        }
         return (
             <div>
                 <div>Submit</div>
