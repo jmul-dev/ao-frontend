c633ab04c609844ec5c7cb83ac2eebc54727ee9d
Neil Harlow <neil@y-designs.com>
2018-08-03 15:03:52 -0700

    updated boot screen with loading animation

--- src/layouts/boot/BootLayout.js
+++ src/layouts/boot/BootLayout.js
@@ -10,16 +10,33 @@ import IpcLogs from '../../modules/electron/components/IpcLogs';
 import { connect } from 'react-redux';
 import Typography from '@material-ui/core/Typography';
 import Snackbar from '@material-ui/core/Snackbar';
-import CircularProgress from '@material-ui/core/CircularProgress';
+import Lottie from 'react-lottie';
+import * as loadingAnimation from './loadingAnimation.json'
 
 
 const BootLayout = ({networkError, states, isElectron}) => (
     <div className="BootLayout">
-        <Typography variant="display1">{'AO'}</Typography>
+        <div className="loading-container" style={{padding: 16}}>
+            <Typography variant="body1" style={{color: '#03B742'}}>{'starting up...'}</Typography>
+            <div style={{position: 'fixed', top: 0, left: 0, right: 0, bottom: 0, background: '#000000', zIndex: -1}}>
+                <Lottie
+                    style={{position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)'}}
+                    height={1000}
+                    width={1000}
+                    options={{
+                        loop: true,
+                        autoplay: true,
+                        animationData: loadingAnimation,
+                        rendererSettings: {
+                            preserveAspectRatio: 'xMidYMid slice'
+                        }
+                    }}
+                />
+            </div>
+        </div>        
         {isElectron ? (
             <IpcLogs />
         ) : null}
-        <CircularProgress />
         <Snackbar
             open={!!networkError}
             message={<span>{'Unable to connect to ao-core, make sure it is running at:'}<br/>{window.AO_CORE_URL}</span>}
