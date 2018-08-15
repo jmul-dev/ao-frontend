215c7e372324b9e9c243f17865c3772b461d088d
Neil Harlow <neil@y-designs.com>
2018-08-15 13:25:27 -0700

    preview url fix

--- src/modules/account/components/AccountVideoListing.js
+++ src/modules/account/components/AccountVideoListing.js
@@ -29,7 +29,7 @@ class AccountVideoListItem extends Component {
                 <Grid container spacing={16} alignItems="center">
                     <Grid item sm={4}>
                         <ButtonBase style={{width: '100%'}}>
-                            <div className="featured-image" style={{backgroundImage: `url(${video.featuredImageUrl})`}}>
+                            <div className="featured-image" style={{backgroundImage: `url(${window.AO_CORE_URL}/${video.featuredImageUrl})`}}>
                                 <PlayIcon className="play-icon" />
                             </div>
                         </ButtonBase>
