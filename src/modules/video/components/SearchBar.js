289c295525064a8dc25dc6cc3774d366d2a4e02c
Neil Harlow <neil@y-designs.com>
2018-09-10 13:35:19 -0700

    search ui mostly complete

--- src/modules/video/components/SearchBar.js
+++ src/modules/video/components/SearchBar.js
@@ -1,13 +1,89 @@
 import React, { Component } from 'react';
 import withSearch from '../containers/withSearch';
+import PropTypes from 'prop-types';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
+import TextField from '@material-ui/core/TextField';
+import '../styles/search-bar.css';
 
 
 class SearchBar extends Component {
+    static propTypes = {
+        // redux bound state
+        searchString: PropTypes.string,
+        searchBarActive: PropTypes.bool,
+        // redux bound actions
+        setSearchBarActive: PropTypes.func.isRequired,
+        updateSearchValue: PropTypes.func.isRequired,
+    }
+    constructor(props) {
+        super(props)
+        this.state = {
+            tempSearchInput: props.searchString
+        }
+    }
+    componentWillReceiveProps(nextProps) {
+        if (!this.props.searchBarActive && nextProps.searchBarActive) {
+            this.inputRef.focus()
+        }
+        if (this.props.searchBarActive && !nextProps.searchBarActive) {
+            this.setState({ tempSearchInput: nextProps.searchString })
+            this.inputRef.blur()
+        }
+    }
+    _closeSearchBar = () => {
+        this.props.setSearchBarActive(false)
+    }
+    _openSearchBar = () => {
+        this.props.setSearchBarActive(true)
+    }
+    _closeAndClearSearch = () => {
+        this.props.setSearchBarActive(false)
+        this.props.updateSearchValue('')
+    }
+    _onSearchInputChange = (event) => {
+        this.setState({ tempSearchInput: event.target.value })
+    }
+    _search = (event) => {
+        if (event)
+            event.preventDefault()
+        this.props.updateSearchValue(this.state.tempSearchInput)
+        this.props.setSearchBarActive(false)
+        // TODO, search
+    }
     render() {
         const { searchString, searchBarActive } = this.props
+        const { tempSearchInput } = this.state
+        const searchBarMinimized = searchString && !searchBarActive
         return (
             <div className="SearchBar">
-                TODO
+                <div className="close-container">
+                    <IconButton style={{ marginRight: '-14px' }} onClick={this._closeSearchBar}>
+                        <CloseIcon />
+                    </IconButton>
+                </div>
+                <form className="input-container" onSubmit={this._search}>
+                    <TextField
+                        className="search-input"
+                        inputRef={inputRef => { this.inputRef = inputRef }}
+                        onFocus={this._openSearchBar}
+                        placeholder="Search videos"
+                        value={tempSearchInput}
+                        onChange={this._onSearchInputChange}
+                        margin="normal"
+                        fullWidth
+                    />
+                    <div className="action-button">
+                        {searchBarMinimized ? (
+                            <IconButton onClick={this._closeAndClearSearch}>
+                                <CloseIcon />
+                            </IconButton>
+                        ) : (
+                            <Button onClick={this._search}>{`Search`}</Button>
+                        )}
+                    </div>
+                </form>
             </div>
         );
     }
--- src/views/browse/BrowseView.js
+++ src/views/browse/BrowseView.js
@@ -1,29 +1,65 @@
 // @flow
 import React, { Component } from 'react';
 import View from '../View';
-import IconButton from '@material-ui/core/IconButton';
-import SearchIcon from '@material-ui/icons/Search';
 import Clock from '../../modules/clock/components/Clock';
 import VideoListing from '../../modules/video/components/VideoListing';
 import EnqueuedVideos from '../../modules/video/components/EnqueuedVideos';
+import SearchBar from '../../modules/video/components/SearchBar';
+import withSearch from '../../modules/video/containers/withSearch';
+import className from 'classnames';
+import IconButton from '@material-ui/core/IconButton';
+import SearchIcon from '@material-ui/icons/Search';
+import Backdrop from '@material-ui/core/Backdrop';
+import Slide from '@material-ui/core/Slide';
 import './browse-view.css';
 
 
-export default class BrowseView extends Component {
+class BrowseView extends Component {
+    _openSearchBar = () => {
+        this.props.setSearchBarActive(true)
+    }
+    _closeSearchBar = () => {
+        this.props.setSearchBarActive(false)
+    }
+    _closeAndClearSearch = () => {
+        this.props.setSearchBarActive(false)
+        this.props.updateSearchValue('')
+    }
     render() {
+        const { searchString, searchBarActive } = this.props
+        const searchActive = !searchBarActive && searchString && searchString.length > 0
+        const containerClasses = className('BrowseView', {
+            'search-input-active': searchBarActive,
+            'search-active': searchActive
+        })
         return (
-            <View className={'BrowseView'} padding="none">
-                <header>
-                    <Clock />
-                    <div style={{marginLeft: 'auto'}}>
-                        <IconButton style={{marginRight: '-14px'}}>
-                            <SearchIcon />
-                        </IconButton>
-                    </div>
-                </header>
+            <View className={containerClasses} padding="none">
+                <Slide direction={'down'} in={!searchBarActive && !searchActive}>
+                    <header>
+                        <Clock />
+                        <div style={{marginLeft: 'auto'}}>
+                            <IconButton style={{marginRight: '-14px'}} onClick={this._openSearchBar}>
+                                <SearchIcon />
+                            </IconButton>
+                        </div>
+                    </header>
+                </Slide>
+                <Slide direction={'down'} in={searchBarActive || searchActive}>
+                    <aside style={{transform: searchActive ? `translateY(-76px)` : undefined}}>
+                        <SearchBar />
+                    </aside>
+                </Slide>
+                <Backdrop
+                    open={searchBarActive} 
+                    className="search-backdrop" 
+                    onClick={this._closeSearchBar}
+                    style={{visibility: searchBarActive ? 'visible' : 'hidden'}}
+                />
                 <VideoListing />
-                <EnqueuedVideos />
+                <EnqueuedVideos />                
             </View>
         );
     }
 }
+
+export default withSearch(BrowseView)
