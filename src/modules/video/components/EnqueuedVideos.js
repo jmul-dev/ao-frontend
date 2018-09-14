b3fcce9dc2ab61f12df8c0b6cd6178b1373bdd06
Neil Harlow <neil@y-designs.com>
2018-09-14 13:49:04 -0700

    started polling on incomplete content to ensure state updates

--- src/modules/video/components/EnqueuedVideos.js
+++ src/modules/video/components/EnqueuedVideos.js
@@ -9,9 +9,9 @@ import { ContentPurchaseState, ContentPurchaseAction, statesPendingUserAction } 
 
 type Props = {
     // redux bound state
-    
+
     // redux bound methods
-    
+
     // graphql props
     incompleteHostedContentQuery: {
         node: Object,
@@ -21,11 +21,41 @@ type Props = {
 }
 
 class EnqueuedVideos extends Component<Props> {
+    constructor() {
+        super()
+        this.state = {
+            polling: false
+        }
+    }
+    componentDidUpdate() {
+        const { incompleteHostedContentQuery } = this.props
+        const numberOfIncomplete = incompleteHostedContentQuery.node ? incompleteHostedContentQuery.node.hostedContent.length : 0
+        // If not polling yet and incomplete videos exist, start polling
+        if (!this.state.polling && numberOfIncomplete > 0) {
+            this._beginPollingEnqueuedVideoContent()
+        } else if (this.state.polling && numberOfIncomplete <= 0) {
+            this._stopPollingEnqueuedVideoContent()
+        }
+    }
+    componentWillUnmount() {
+        if ( this.state.polling )
+            this.props.incompleteHostedContentQuery.stopPolling()
+    }
+    _beginPollingEnqueuedVideoContent = () => {
+        this.setState({ polling: true }, () => {
+            this.props.incompleteHostedContentQuery.startPolling(1500)
+        })
+    }
+    _stopPollingEnqueuedVideoContent = () => {
+        this.setState({ polling: false }, () => {
+            this.props.incompleteHostedContentQuery.stopPolling()
+        })
+    }
     render() {
         const { loading, error, node } = this.props.incompleteHostedContentQuery
-        if ( loading || error )
+        if (loading || error)
             return null;
-        if ( !node || (node && !node.hostedContent) )
+        if (!node || (node && !node.hostedContent))
             return null;  // no incomplete hosted content
         return (
             <div className="EnqueuedVideos">
@@ -36,28 +66,28 @@ class EnqueuedVideos extends Component<Props> {
                         </li>
                     ))}
                 </ul>
-            </div>            
+            </div>
         )
     }
 }
 
-const EnqueuedVideoListItem = withTheme()(({theme, content, ...props}) => {
+const EnqueuedVideoListItem = withTheme()(({ theme, content, ...props }) => {
     const actionRequired = statesPendingUserAction.indexOf(content.state) > -1
     return (
-        <div className="EnqueuedVideo" style={{backgroundColor: actionRequired ? theme.palette.primary.main : theme.palette.background.default}}>
-            <div style={{overflow: 'hidden', marginRight: 16}}>
+        <div className="EnqueuedVideo" style={{ backgroundColor: actionRequired ? theme.palette.primary.main : theme.palette.background.default }}>
+            <div style={{ overflow: 'hidden', marginRight: 16 }}>
                 <Typography variant="subheading" gutterBottom noWrap>{content.title}</Typography>
                 <Typography variant="body1" component="div" className="action-status">
                     <ContentPurchaseState content={content} />
                 </Typography>
             </div>
-            <div style={{marginLeft: 'auto'}}>
-                <div className="featured-image" style={{backgroundImage: `url(${window.AO_CORE_URL}/${content.featuredImageUrl})`}}>
+            <div style={{ marginLeft: 'auto' }}>
+                <div className="featured-image" style={{ backgroundImage: `url(${window.AO_CORE_URL}/${content.featuredImageUrl})` }}>
                     {actionRequired ? (
-                        <ContentPurchaseAction content={content}>{({action, actionCopy, loading}) => (
+                        <ContentPurchaseAction content={content}>{({ action, actionCopy, loading }) => (
                             <ButtonBase className="action-button" disabled={!action || loading} onClick={action}>
                                 <div className="action-text">
-                                    <Typography variant="body1">{actionCopy}</Typography>                                
+                                    <Typography variant="body1">{actionCopy}</Typography>
                                 </div>
                             </ButtonBase>
                         )}</ContentPurchaseAction>
