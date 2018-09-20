cad3ccab5fb8fa3494935b886a406378c9a17643
Neil Harlow <neil@y-designs.com>
2018-09-20 16:46:09 -0700

    comparison table component complete (aside from copy)

--- src/modules/ico/components/ComparisonTable.js
+++ src/modules/ico/components/ComparisonTable.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
@@ -62,7 +62,9 @@ const styles = theme => ({
         background: theme.palette.grey['400'],
         color: 'white',
         height: 70,
-        borderLeft: 0
+        borderLeft: 0,
+        fontSize: `1rem`,
+        fontWeight: 'bold'
     },
     cellAo: {
         background: lighten(theme.palette.primary.main, 0.64),
@@ -70,23 +72,47 @@ const styles = theme => ({
     },
     tr: {
         verticalAlign: 'top',
+        cursor: 'pointer',
         '&:hover > *:last-child': {
             background: lighten(theme.palette.primary.main, 0.5),
-        },
+        },        
         '&:hover > *:first-child': {
             background: theme.palette.grey['500'],
-        }
+        },
+    },
+    trSelected: {
+        cursor: 'default',
+        '& > *': {
+            background: theme.palette.grey['300'],
+        },
+        '& > *:last-child, &:hover > *:last-child': {
+            background: lighten(theme.palette.primary.main, 0.35),
+        },
+        '& > *:first-child, &:hover > *:first-child': {
+            background: theme.palette.grey['600'],
+        },
+    },
+    expansionDetails: {
+        marginTop: theme.spacing.unit * 3,
     }
 });
 
 let id = 0;
-function createData(label, usd, bitcoin, ao) {
+function createData(label, usd, bitcoin, ao, usdExpanded, bitcoinExpanded, aoExpanded) {
     id += 1;
-    return { id, label, usd, bitcoin, ao };
+    return { id, label, usd, bitcoin, ao, usdExpanded, bitcoinExpanded, aoExpanded };
 }
 
 const rows = [
-    createData('Classification', 'Currency', 'Cryptocurrency', 'Information-currency'),
+    createData(
+        'Classification', 
+        'Currency', 
+        'Cryptocurrency', 
+        'Information-currency',
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, tellus sit amet vestibulum consequat, arcu ex pellentesque augue, ac vehicula nunc magna eget nisi. Cras maximus eu augue volutpat vulputate. Donec efficitur, risus vitae sagittis dapibus, justo nibh posuere metus, quis euismod quam risus vitae ipsum.',
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, tellus sit amet vestibulum consequat, arcu ex pellentesque augue, ac vehicula nunc magna eget nisi. Cras maximus eu augue volutpat vulputate. Donec efficitur, risus vitae sagittis dapibus, justo nibh posuere metus, quis euismod quam risus vitae ipsum.',
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, tellus sit amet vestibulum consequat, arcu ex pellentesque augue, ac vehicula nunc magna eget nisi. Cras maximus eu augue volutpat vulputate. Donec efficitur, risus vitae sagittis dapibus, justo nibh posuere metus, quis euismod quam risus vitae ipsum.',
+    ),
     createData('How it works', '• A standard unit of value', '• A standard unit of value\n• Secured by cryptography', '• A standard unit of value\n• Secured by cryptography\n• Backed by the value of its data'),
     createData('Created by', 'Debt', 'Witnesses', 'Information'),
     createData('Distributed by', 'Banks', 'Witnesses', 'Content creators'),
@@ -98,54 +124,89 @@ const rows = [
     createData('Decisions made by', 'Central banks', 'Witnesses', 'All participants'),
 ];
 
-function ComparisonTable(props) {
-    const { classes } = props;
-    return (
-        <Paper className={classes.root} elevation={0}>
-            <Table className={classes.table}>
-                <TableHead>
-                    <TableRow className={classes.thRow}>
-                        <TableCell padding="dense" color="primary" className={`${classes.thCurrency} ${classes.cell}`}>
-                            <Typography variant="title" style={{color: 'white', marginRight: 'auto'}}>{`Currency`}</Typography>
-                        </TableCell>
-                        <TableCell padding="dense" className={`${classes.thUsd} ${classes.cell}`}>
-                            <div className={classes.thFlex}>
-                                <Typography variant="title" style={{color: 'white', marginRight: 'auto'}}>{`USD`}</Typography>
-                                <UsdIcon />
-                            </div>
-                            <Typography variant="caption" className={classes.thLabel}>{`US Dollar`}</Typography>
-                        </TableCell>
-                        <TableCell padding="dense" className={`${classes.thBitcoin} ${classes.cell}`}>
-                            <div className={classes.thFlex}>
-                                <Typography variant="title" style={{color: 'white', marginRight: 'auto'}}>{`BTC`}</Typography>
-                                <BitcoinIcon />
-                            </div>
-                            <Typography variant="caption" className={classes.thLabel}>{`Bitcoin`}</Typography>
-                        </TableCell>
-                        <TableCell padding="dense" className={`${classes.thAo} ${classes.cell}`}>
-                            <div className={classes.thFlex}>
-                                <Typography variant="title" style={{color: 'white', marginRight: 'auto'}}>{`AO`}</Typography>
-                                <LogoIcon width={25} height={25} />
-                            </div>
-                            <Typography variant="caption" className={classes.thLabel}>{`AO & AO+`}</Typography>
-                        </TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {rows.map(row => {
-                        return (
-                            <TableRow key={row.id} className={classes.tr} hover>
-                                <TableCell component="th" scope="row" className={`${classes.cell} ${classes.cellLabel}`}>{row.label}</TableCell>
-                                <TableCell className={classes.cell}>{row.usd}</TableCell>
-                                <TableCell className={classes.cell}>{row.bitcoin}</TableCell>
-                                <TableCell className={`${classes.cell} ${classes.cellAo}`}>{row.ao}</TableCell>
-                            </TableRow>
-                        );
-                    })}
-                </TableBody>
-            </Table>
-        </Paper>
-    );
+class ComparisonTable extends Component {
+    constructor() {
+        super()
+        this.state = {
+            selectedRowId: null
+        }
+    }
+    _onRowClick = (event, id) => {
+        this.setState({
+            selectedRowId: this.state.selectedRowId === id ? null : id
+        })
+    }
+    render() {
+        const { classes } = this.props;
+        return (
+            <Paper className={classes.root} elevation={0}>
+                <Table className={classes.table}>
+                    <TableHead>
+                        <TableRow className={classes.thRow}>
+                            <TableCell padding="dense" color="primary" className={`${classes.thCurrency} ${classes.cell}`}>
+                                <Typography variant="title" style={{color: 'white', marginRight: 'auto'}}>{`Currency`}</Typography>
+                            </TableCell>
+                            <TableCell padding="dense" className={`${classes.thUsd} ${classes.cell}`}>
+                                <div className={classes.thFlex}>
+                                    <Typography variant="title" style={{color: 'white', marginRight: 'auto'}}>{`USD`}</Typography>
+                                    <UsdIcon />
+                                </div>
+                                <Typography variant="caption" className={classes.thLabel}>{`US Dollar`}</Typography>
+                            </TableCell>
+                            <TableCell padding="dense" className={`${classes.thBitcoin} ${classes.cell}`}>
+                                <div className={classes.thFlex}>
+                                    <Typography variant="title" style={{color: 'white', marginRight: 'auto'}}>{`BTC`}</Typography>
+                                    <BitcoinIcon />
+                                </div>
+                                <Typography variant="caption" className={classes.thLabel}>{`Bitcoin`}</Typography>
+                            </TableCell>
+                            <TableCell padding="dense" className={`${classes.thAo} ${classes.cell}`}>
+                                <div className={classes.thFlex}>
+                                    <Typography variant="title" style={{color: 'white', marginRight: 'auto'}}>{`AO`}</Typography>
+                                    <LogoIcon width={25} height={25} />
+                                </div>
+                                <Typography variant="caption" className={classes.thLabel}>{`AO & AO+`}</Typography>
+                            </TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {rows.map(row => {
+                            const rowIsSelected = this.state.selectedRowId === row.id
+                            return (
+                                <TableRow 
+                                    key={row.id} 
+                                    className={`${classes.tr} ${rowIsSelected ? classes.trSelected : ''}`} 
+                                    hover={!rowIsSelected}
+                                    onClick={(event) => this._onRowClick(event, row.id)}
+                                    selected={rowIsSelected}
+                                    >
+                                    <TableCell component="th" scope="row" className={`${classes.cell} ${classes.cellLabel}`}>{row.label}</TableCell>
+                                    <TableCell className={classes.cell}>
+                                        {row.usd}
+                                        {rowIsSelected ? (
+                                            <div className={classes.expansionDetails}>{row.usdExpanded}</div>
+                                        ) : null}
+                                    </TableCell>
+                                    <TableCell className={classes.cell}>
+                                        {row.bitcoin}
+                                        {rowIsSelected ? (
+                                            <div className={classes.expansionDetails}>{row.bitcoinExpanded}</div>
+                                        ) : null}
+                                    </TableCell>
+                                    <TableCell className={`${classes.cell} ${classes.cellAo}`}>
+                                        {row.ao}
+                                        {rowIsSelected ? (
+                                            <div className={classes.expansionDetails}>{row.aoExpanded}</div>
+                                        ) : null}
+                                    </TableCell>
+                                </TableRow>
+                            );
+                        })}
+                    </TableBody>
+                </Table>
+            </Paper>
+        );
+    }
 }
 
 ComparisonTable.propTypes = {
