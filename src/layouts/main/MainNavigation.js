b505303662f08202e383bd72f28b840e203f75c5
Neil Harlow <neil@y-designs.com>
2018-10-11 13:28:08 -0700

    work on the main nav update with wallet balances

--- src/layouts/main/MainNavigation.js
+++ src/layouts/main/MainNavigation.js
@@ -20,21 +20,33 @@ import navIconDaoSrc from '../../assets/nav-icon-dao.svg';
 import navIconDevelopersSrc from '../../assets/nav-icon-developers.svg';
 import DownloadsList from '../../modules/downloads/components/DownloadsList';
 import { APP_STATES } from '../../store/app.reducer';
+import Account from '../../modules/account/components/Account';
 
 
 const openMetamask = () => {
     window.chrome.ipcRenderer.send('open-metamask-popup')
 }
 
-const MainNavigation = ({ isElectron, offcanvas, showDownloads, overlayViewsActive, classes }) => (
+const MainNavigation = ({ isElectron, offcanvas, ethAddress, showDownloads, overlayViewsActive, classes }) => (
     <nav className={classnames('MainNavigation', { offcanvas })}>
-        <div style={{ display: 'flex' }}>
-            <div className={classes.logoContainer}>
-                <Link to="/app">
-                    <LogoIcon />
-                </Link>
-            </div>
+        <div className={classes.container}>
             <ul className={classes.navList}>
+                <li className={`${classes.navListItem} ${classes.walletListItem}`}>
+                    <NavLink className={`${classes.navListItemLink} ${classes.navListItemWallet}`} to="/app/view/wallet" replace={overlayViewsActive}>
+                        <div className={classes.navListItemWalletTitle}>
+                            {ethAddress ? (
+                                <Account display="ethIcon" className={classes.navLinkIcon} size={20} />
+                            ) : (
+                                <img src={navIconWalletSrc} className={classes.navLinkIcon} alt="Wallet" />  
+                            )}                            
+                            <Typography variant="caption" className={classes.navLinkCopy}>{'My Wallet'}</Typography>
+                        </div>
+                        <div className={classes.navListItemWalletBalances}>
+                            <Typography variant="caption" className={classes.walletBalanceCopy}>{'0 kilo AO+'}</Typography>
+                            <Typography variant="caption" className={classes.walletBalanceCopy}>{'0 kilo AO'}</Typography>
+                        </div>                        
+                    </NavLink>
+                </li>
                 <li className={classes.navListItem}>
                     <NavLink className={classes.navListItemLink} to="/app" exact>
                         <img src={navIconBrowseSrc} className={classes.navLinkIcon} alt="Browse" />
@@ -58,13 +70,7 @@ const MainNavigation = ({ isElectron, offcanvas, showDownloads, overlayViewsActi
                         <img src={navIconUploadSrc} className={classes.navLinkIcon} alt="Upload Content" />
                         <Typography variant="caption" className={classes.navLinkCopy}>{'Upload Content'}</Typography>
                     </NavLink>
-                </li>
-                <li className={classes.navListItem}>
-                    <NavLink className={classes.navListItemLink} to="/app/view/wallet" replace={overlayViewsActive}>
-                        <img src={navIconWalletSrc} className={classes.navLinkIcon} alt="Wallet" />
-                        <Typography variant="caption" className={classes.navLinkCopy}>{'My Wallet'}</Typography>
-                    </NavLink>
-                </li>
+                </li>                
                 <li className={classes.navListItem}>
                     <NavLink className={classes.navListItemLink} to="/app/view/settings" replace={overlayViewsActive}>
                         <img src={navIconSettingsSrc} className={classes.navLinkIcon} alt="Settings" />
@@ -108,16 +114,20 @@ const MainNavigation = ({ isElectron, offcanvas, showDownloads, overlayViewsActi
 )
 
 const styles = ({palette, spacing}) => ({
-    logoContainer: {
-        marginTop: 40,
-        marginBottom: 48,
-        paddingLeft: spacing.unit * 2,
+    container: {
+        height: '100%',
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'column',
     },
     navList: {
         listStyle: 'none',
         margin: 0,
         padding: 0,
     },
+        walletListItem: {
+            borderBottom: `1px solid ${palette.divider}`
+        },
         navListItem: {
             overflow: 'hidden',
         },
@@ -135,6 +145,23 @@ const styles = ({palette, spacing}) => ({
                 opacity: 1,
             },
         },
+            navListItemWallet: {
+                flexDirection: 'column',
+                alignItems: 'flex-start',
+                paddingTop: 32,
+                paddingBottom: 24,
+            },
+            navListItemWalletTitle: {
+                display: 'flex',
+                alignItems: 'center',
+            },
+            navListItemWalletBalances: {
+                paddingLeft: 29,
+                marginTop: 8,
+            },
+            walletBalanceCopy: {
+                color: '#777777',
+            },
         navLinkCopy: {
             fontWeight: 'bold',
         },
@@ -156,6 +183,7 @@ const mapStateToProps = (store) => {
         showDownloads: store.app.ethAddress && store.app.states[APP_STATES.CORE_READY],
         overlayViewsActive: store.router.location.pathname.indexOf('/app/view') > -1,
         isElectron: store.electron.isElectron,
+        ethAddress: store.app.ethAddress,
     }
 }
 
