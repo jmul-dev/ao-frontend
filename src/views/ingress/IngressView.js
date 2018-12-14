e1098648e1ad72ba95c5d13db103c857bdbc0129
Neil Harlow <nharlow89@gmail.com>
2018-12-13 17:12:23 -0800

    modified ingress window to only work when signed into metamask, also fixed the creation of the dapp BrowserWindow. added some css to hide scrollbars in electron window

--- src/views/ingress/IngressView.js
+++ src/views/ingress/IngressView.js
@@ -1,24 +1,33 @@
 import React, { PureComponent } from 'react';
 import View from '../View';
-import Typography from '@material-ui/core/Typography';
+import withEthAddress from '../../modules/account/containers/withEthAddress';
+import AccountRequired from '../../modules/account/components/AccountRequired';
 
 
-export default class IngressView extends PureComponent {
+class IngressView extends PureComponent {
     constructor() {
         super()
         this.state = {
-            isElectron: !!(window.chrome && window.chrome.ipcRenderer)
+            isElectron: !!(window.chrome && window.chrome.ipcRenderer),
         }
     }
     componentDidMount() {
-        if ( this.state.isElectron ) {
-            window.chrome.ipcRenderer.send('open-metamask-notification')
+        if ( this.state.isElectron && this.props.ethAddress ) {
             window.chrome.ipcRenderer.send('OPEN_DAPP_WINDOW', "https://www.ingress.one")
         }
     }
     componentWillUnmount() {
         if ( this.state.isElectron ) {
             window.chrome.ipcRenderer.send('CLOSE_DAPP_WINDOW')
+        }
+    }
+    componentDidUpdate(prevProps) {
+        if ( this.state.isElectron ) {
+            if (prevProps.ethAddress && !this.props.ethAddress) {
+                window.chrome.ipcRenderer.send('CLOSE_DAPP_WINDOW')
+            } else if (!prevProps.ethAddress && this.props.ethAddress) {
+                window.chrome.ipcRenderer.send('OPEN_DAPP_WINDOW', "https://www.ingress.one")
+            }
         }
     }
     render() {
@@ -31,10 +40,18 @@ export default class IngressView extends PureComponent {
                             src="https://www.ingress.one" 
                             style={{height: '100%', width: '100%', border: 0}} 
                         />
-                    )}        
-                    {/*  */}
+                    )}
+                    {isElectron && (
+                        <div style={{height: '100%', width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+                            <AccountRequired>
+                                <div>{/*placeholder*/}</div>
+                            </AccountRequired>
+                        </div>
+                    )}
                 </section>
             </View>
         );
     }
 }
+
+export default withEthAddress(IngressView)
