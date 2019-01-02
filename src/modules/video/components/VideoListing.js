fa34f30540ee58e375639ce8d5d4139d8ed7a933
Neil Harlow <neil@y-designs.com>
2019-01-02 12:40:55 -0800

    fixed virtualized grid (browse view) so that it is actually virtualizing now. we were previously setting the height of the grid to match the total height if all videos were rendered which was causing jsut that

--- src/modules/video/components/VideoListing.js
+++ src/modules/video/components/VideoListing.js
@@ -1,21 +1,27 @@
 // @flow
-import React, { Component } from 'react';
-import { AutoSizer, ColumnSizer, Grid } from 'react-virtualized';
-import 'react-virtualized/styles.css';
-import Button from '@material-ui/core/Button';
-import ButtonBase from '@material-ui/core/ButtonBase';
-import ArrowBackIcon from '@material-ui/icons/ArrowBack';
-import Typography from '@material-ui/core/Typography';
-import '../styles/video-listing.css';
-import TeaserListing from './TeaserListing';
-import { CSSTransition } from 'react-transition-group';
-import withVideos from '../containers/withVideos';
-import Fade from '@material-ui/core/Fade';
-import OfflineIcon from '@material-ui/icons/CloudOff';
-import Tooltip from '@material-ui/core/Tooltip';
+import React, { Component } from "react";
+import { AutoSizer, ColumnSizer, Grid } from "react-virtualized";
+import "react-virtualized/styles.css";
+import Button from "@material-ui/core/Button";
+import ButtonBase from "@material-ui/core/ButtonBase";
+import ArrowBackIcon from "@material-ui/icons/ArrowBack";
+import Typography from "@material-ui/core/Typography";
+import "../styles/video-listing.css";
+import TeaserListing from "./TeaserListing";
+import { CSSTransition } from "react-transition-group";
+import withVideos from "../containers/withVideos";
+import Fade from "@material-ui/core/Fade";
+import OfflineIcon from "@material-ui/icons/CloudOff";
+import Tooltip from "@material-ui/core/Tooltip";
 
-
-const propertySelection = (({ top, right, bottom, left, width, height }) => ({ top, right, bottom, left, width, height }))
+const propertySelection = ({ top, right, bottom, left, width, height }) => ({
+    top,
+    right,
+    bottom,
+    left,
+    width,
+    height
+});
 
 type Props = {
     setTeaserListingState: Function,
@@ -24,60 +30,85 @@ type Props = {
     activeVideo?: Object,
     videos: Function,
     videosLoading: boolean,
-    videosError?: Error,
+    videosError?: Error
 };
 
 class VideoListing extends Component<Props> {
     props: Props;
     constructor() {
-        super()
+        super();
         this.state = {
             activeTeaserVideoIndex: undefined,
-            enteredTeaser: false,
-        }
-        this._boundEscKeyListener = this._exitFullscreenEscKeyListener.bind(this)
+            enteredTeaser: false
+        };
+        this._boundEscKeyListener = this._exitFullscreenEscKeyListener.bind(
+            this
+        );
     }
     componentDidMount() {
         document.addEventListener("keydown", this._boundEscKeyListener, false);
     }
     componentWillUnmount() {
-        document.removeEventListener("keydown", this._boundEscKeyListener, false);
+        document.removeEventListener(
+            "keydown",
+            this._boundEscKeyListener,
+            false
+        );
     }
     componentWillReceiveProps(nextProps) {
         if (this.props.searchString !== nextProps.searchString) {
             // TODO: may need to move into componentDidUpdate if props have not propogated to container withVideos
-            this.props.videos.refetch()
+            this.props.videos.refetch();
         }
     }
-    _exitFullscreenEscKeyListener = (event) => {
-        const { activeVideo, teaserListingActive, setActiveVideo, setTeaserListingState } = this.props
-        if ( event.keyCode === 27 ) {  // esc
-            if ( activeVideo )
-                setActiveVideo()
-            else if ( teaserListingActive )
-                setTeaserListingState({isActive: false})
+    _exitFullscreenEscKeyListener = event => {
+        const {
+            activeVideo,
+            teaserListingActive,
+            setActiveVideo,
+            setTeaserListingState
+        } = this.props;
+        if (event.keyCode === 27) {
+            // esc
+            if (activeVideo) setActiveVideo();
+            else if (teaserListingActive)
+                setTeaserListingState({ isActive: false });
         }
-    }
+    };
     // Used when there are no videos
     _renderPlaceholderView() {
         return (
             <div className="VideoListing placeholder">
-                <Typography className="placeholder-title" variant="display1">{`Welcome to AO`}</Typography>
-                <Typography className="placeholder-description" variant="body1">{`Content will begin to show up here as it is discovered within the AO network`}</Typography>
+                <Typography
+                    className="placeholder-title"
+                    variant="display1"
+                >{`Welcome to AO`}</Typography>
+                <Typography
+                    className="placeholder-description"
+                    variant="body1"
+                >{`Content will begin to show up here as it is discovered within the AO network`}</Typography>
             </div>
-        )
+        );
     }
     render() {
-        const { videos, videosLoading, teaserListingActive, activeVideo, setActiveVideo, setTeaserListingState } = this.props
-        const rowCount = videos.videos ? Math.ceil(videos.videos.length / 3) : 0
-        if (videosLoading && !videos.videos)
-            return null
+        const {
+            videos,
+            videosLoading,
+            teaserListingActive,
+            activeVideo,
+            setActiveVideo,
+            setTeaserListingState
+        } = this.props;
+        const rowCount = videos.videos
+            ? Math.ceil(videos.videos.length / 3)
+            : 0;
+        if (videosLoading && !videos.videos) return null;
         if (videos.videos && videos.videos.length === 0)
-            return this._renderPlaceholderView()
+            return this._renderPlaceholderView();
         return (
-            <div className="VideoListing">
-                <AutoSizer disableHeight>
-                    {({ width }) => (
+            <div className="VideoListing" style={{ height: "100%" }}>
+                <AutoSizer>
+                    {({ width, height }) => (
                         <ColumnSizer
                             width={width}
                             columnMaxWidth={window.innerWidth / 3}
@@ -85,17 +116,25 @@ class VideoListing extends Component<Props> {
                             columnCount={3}
                             key="GridColumnSizer"
                         >
-                            {({ adjustedWidth, columnWidth, registerChild }) => (
+                            {({
+                                adjustedWidth,
+                                columnWidth,
+                                registerChild
+                            }) => (
                                 <Grid
-                                    ref={ref => this._grid = ref}
+                                    ref={ref => (this._grid = ref)}
                                     cellRenderer={this._renderCell}
                                     columnCount={3}
                                     columnWidth={columnWidth}
                                     width={adjustedWidth}
                                     rowCount={rowCount}
                                     rowHeight={adjustedWidth / 3}
-                                    height={adjustedWidth / 3 * rowCount + 200}
-                                    style={{ marginTop: 200, marginBottom: 200 }}
+                                    height={height}
+                                    style={{
+                                        paddingTop: 200,
+                                        paddingBottom: 200
+                                    }}
+                                    overscanRowCount={5}
                                 />
                             )}
                         </ColumnSizer>
@@ -110,17 +149,28 @@ class VideoListing extends Component<Props> {
                     onEntered={this._onEnteredTeaserListing}
                 >
                     <div className="teaser-modal">
-                        <div className="teaser-modal-backdrop"></div>
+                        <div className="teaser-modal-backdrop" />
                         <TeaserListing
                             videos={videos.videos}
-                            activeTeaserVideoIndex={this.state.activeTeaserVideoIndex}
-                            activeTeaserVideoCellPosition={this.state.activeTeaserVideoCellPosition}
+                            activeTeaserVideoIndex={
+                                this.state.activeTeaserVideoIndex
+                            }
+                            activeTeaserVideoCellPosition={
+                                this.state.activeTeaserVideoCellPosition
+                            }
                             activeFullscreenVideo={this.props.activeVideo}
-                            updateActiveVideoIndex={this._setActiveTeaserVideoIndex}
+                            updateActiveVideoIndex={
+                                this._setActiveTeaserVideoIndex
+                            }
                             enteredTeaser={this.state.enteredTeaser}
                         />
                         <nav className="video-navigation">
-                            <Button varient="contained" onClick={() => setTeaserListingState({ isActive: false })}>
+                            <Button
+                                varient="contained"
+                                onClick={() =>
+                                    setTeaserListingState({ isActive: false })
+                                }
+                            >
                                 <ArrowBackIcon />
                                 <Typography variant="body1">{`back to browse`}</Typography>
                             </Button>
@@ -134,9 +184,12 @@ class VideoListing extends Component<Props> {
                     unmountOnExit
                 >
                     <div className="playback-modal">
-                        <div className="playback-modal-backdrop"></div>
+                        <div className="playback-modal-backdrop" />
                         <nav className="video-navigation">
-                            <Button varient="contained" onClick={() => setActiveVideo(undefined)}>
+                            <Button
+                                varient="contained"
+                                onClick={() => setActiveVideo(undefined)}
+                            >
                                 <ArrowBackIcon />
                                 <Typography variant="body1">{`back to info`}</Typography>
                             </Button>
@@ -147,89 +200,123 @@ class VideoListing extends Component<Props> {
         );
     }
     _renderCell = ({ columnIndex, key, rowIndex, style }) => {
-        const videoIndex = rowIndex * 3 + columnIndex
-        const video = this.props.videos.videos[videoIndex]
-        const isActive = this.state.activeTeaserVideoIndex === videoIndex
+        const videoIndex = rowIndex * 3 + columnIndex;
+        const video = this.props.videos.videos[videoIndex];
+        const isActive = this.state.activeTeaserVideoIndex === videoIndex;
         return video ? (
-            <div className="Cell" key={key} style={{ ...style, opacity: isActive ? 0 : 1 }}>
-                <VideoListingCellCard 
+            <div
+                className={`Cell video-${videoIndex}`}
+                key={key}
+                style={{ ...style, opacity: isActive ? 0 : 1 }}
+            >
+                <VideoListingCellCard
                     video={video}
                     className="clickable"
-                    onClick={this._enterTeaserListingAtVideo.bind(this, videoIndex)} 
+                    onClick={this._enterTeaserListingAtVideo.bind(
+                        this,
+                        videoIndex
+                    )}
                 />
             </div>
         ) : null;
-    }
+    };
     _enterTeaserListingAtVideo = (videoIndex, event) => {
-        const activeTeaserVideoCellPosition = propertySelection(event.currentTarget.getBoundingClientRect())
-        this.props.setTeaserListingState({ isActive: true })
-        this.setState({
-            activeTeaserVideoIndex: videoIndex,
-            activeTeaserVideoCellPosition,
-        })
-        // Disable scrolling 
-        document.querySelector('.BrowseView').style.overflow = 'hidden'
-    }
-    _setActiveTeaserVideoIndex = (videoIndex) => {
-        const targetCell = document.querySelectorAll('.VideoListing .Cell')[videoIndex]
-        if (!targetCell)
-            return console.warn(`Could not find .Cell in VideoListing at index ${videoIndex}`)
-        const targetCellClickable = targetCell.querySelector('.clickable')
-        const activeTeaserVideoCellPosition = propertySelection(targetCellClickable.getBoundingClientRect())
+        const activeTeaserVideoCellPosition = propertySelection(
+            event.currentTarget.getBoundingClientRect()
+        );
+        this.props.setTeaserListingState({ isActive: true });
         this.setState({
             activeTeaserVideoIndex: videoIndex,
             activeTeaserVideoCellPosition
-        })
-    }
+        });
+        // Disable scrolling
+        document.querySelector(".BrowseView").style.overflow = "hidden";
+    };
+    _setActiveTeaserVideoIndex = videoIndex => {
+        const targetCell = document.querySelector(
+            `.VideoListing .Cell.video-${videoIndex}`
+        );
+        if (targetCell) {
+            const targetCellClickable = targetCell.querySelector(".clickable");
+            const activeTeaserVideoCellPosition = propertySelection(
+                targetCellClickable.getBoundingClientRect()
+            );
+            this.setState({
+                activeTeaserVideoIndex: videoIndex,
+                activeTeaserVideoCellPosition
+            });
+        } else {
+            console.warn(
+                `Could not find .Cell in VideoListing at index ${videoIndex}, only effects animation`
+            );
+            this.setState({
+                activeTeaserVideoIndex: videoIndex
+            });
+        }
+    };
     _unsetActiveTeaserVideo = () => {
         this.setState({
             activeTeaserVideoIndex: undefined,
             activeTeaserVideoCellPosition: undefined,
             enteredTeaser: false
-        })
-        // Enable scrolling 
-        document.querySelector('.BrowseView').style.overflow = 'auto'
-    }
+        });
+        // Enable scrolling
+        document.querySelector(".BrowseView").style.overflow = "auto";
+    };
     _onEnteredTeaserListing = () => {
-        this.setState({ enteredTeaser: true })
-    }
+        this.setState({ enteredTeaser: true });
+    };
 }
 
 class VideoListingCellCard extends Component {
     constructor() {
-        super()
+        super();
         this.state = {
             imageLoaded: false
-        }
+        };
     }
     render() {
-        const { video, ...props } = this.props
+        const { video, ...props } = this.props;
         return (
             <Fade in={this.state.imageLoaded}>
-                <div {...props} style={{opacity: video.recentlySeenHostsCount > 0 ? 1 : 0.25}}>
+                <div
+                    {...props}
+                    style={{
+                        opacity: video.recentlySeenHostsCount > 0 ? 1 : 0.25
+                    }}
+                >
                     <img
-                        src={`${process.env.REACT_APP_AO_CORE_URL}/${video.featuredImageUrl}`}
+                        src={`${process.env.REACT_APP_AO_CORE_URL}/${
+                            video.featuredImageUrl
+                        }`}
                         alt={video.title}
-                        onLoad={() => this.setState({imageLoaded: true})}
-                        onError={() => this.setState({imageLoaded: true})}
-                        style={{position: 'absolute', visibility: 'hidden'}}
+                        onLoad={() => this.setState({ imageLoaded: true })}
+                        onError={() => this.setState({ imageLoaded: true })}
+                        style={{ position: "absolute", visibility: "hidden" }}
                     />
                     <ButtonBase
                         className="cover-image"
-                        style={{ backgroundImage: `url(${process.env.REACT_APP_AO_CORE_URL}/${video.featuredImageUrl})` }}
-                    ></ButtonBase>
-                    <Typography variant="subheading" style={{display: 'flex'}}>
+                        style={{
+                            backgroundImage: `url(${
+                                process.env.REACT_APP_AO_CORE_URL
+                            }/${video.featuredImageUrl})`
+                        }}
+                    />
+                    <Typography
+                        variant="subheading"
+                        style={{ display: "flex" }}
+                    >
                         {video.title}
                         {video.recentlySeenHostsCount < 1 && (
                             <Tooltip title="Unable to find a host for this piece of content">
-                                <OfflineIcon style={{marginLeft: 'auto'}}/>
+                                <OfflineIcon style={{ marginLeft: "auto" }} />
                             </Tooltip>
                         )}
-                    </Typography>                    
+                    </Typography>
                 </div>
             </Fade>
-        )
+        );
     }
 }
 
-export default withVideos(VideoListing)
+export default withVideos(VideoListing);
--- src/views/browse/BrowseView.js
+++ src/views/browse/BrowseView.js
@@ -1,59 +1,70 @@
-import React, { Component } from 'react';
-import View from '../View';
-import Clock from '../../modules/clock/components/Clock';
-import VideoListing from '../../modules/video/components/VideoListing';
-import SearchBar from '../../modules/video/components/SearchBar';
-import withSearch from '../../modules/video/containers/withSearch';
-import className from 'classnames';
-import IconButton from '@material-ui/core/IconButton';
-import SearchIcon from '@material-ui/icons/Search';
-import Backdrop from '@material-ui/core/Backdrop';
-import Slide from '@material-ui/core/Slide';
-import Fade from '@material-ui/core/Fade';
-import './browse-view.css';
-import Stats from '../../modules/stats/components/Stats';
-
+import React, { Component } from "react";
+import View from "../View";
+import Clock from "../../modules/clock/components/Clock";
+import VideoListing from "../../modules/video/components/VideoListing";
+import SearchBar from "../../modules/video/components/SearchBar";
+import withSearch from "../../modules/video/containers/withSearch";
+import className from "classnames";
+import IconButton from "@material-ui/core/IconButton";
+import SearchIcon from "@material-ui/icons/Search";
+import Backdrop from "@material-ui/core/Backdrop";
+import Slide from "@material-ui/core/Slide";
+import Fade from "@material-ui/core/Fade";
+import "./browse-view.css";
+import Stats from "../../modules/stats/components/Stats";
 
 class BrowseView extends Component {
     _openSearchBar = () => {
-        this.props.setSearchBarActive(true)
-    }
+        this.props.setSearchBarActive(true);
+    };
     _closeSearchBar = () => {
-        this.props.setSearchBarActive(false)
-    }
+        this.props.setSearchBarActive(false);
+    };
     _closeAndClearSearch = () => {
-        this.props.setSearchBarActive(false)
-        this.props.updateSearchValue('')
-    }
+        this.props.setSearchBarActive(false);
+        this.props.updateSearchValue("");
+    };
     render() {
-        const { searchString, searchBarActive, ethAddress } = this.props
-        const searchActive = !searchBarActive && searchString && searchString.length > 0
-        const containerClasses = className('BrowseView', {
-            'search-input-active': searchBarActive,
-            'search-active': searchActive
-        })
+        const { searchString, searchBarActive, ethAddress } = this.props;
+        const searchActive =
+            !searchBarActive && searchString && searchString.length > 0;
+        const containerClasses = className("BrowseView", {
+            "search-input-active": searchBarActive,
+            "search-active": searchActive
+        });
         return (
             <View className={containerClasses} padding="none">
                 <Fade in={!searchBarActive && !searchActive}>
                     <header>
                         {/* <Clock /> */}
-                        <div style={{marginLeft: 'auto'}}>
-                            <IconButton style={{marginRight: '-14px'}} onClick={this._openSearchBar}>
+                        <div style={{ marginLeft: "auto" }}>
+                            <IconButton
+                                style={{ marginRight: "-14px" }}
+                                onClick={this._openSearchBar}
+                            >
                                 <SearchIcon />
                             </IconButton>
                         </div>
                     </header>
                 </Fade>
-                <Slide direction={'down'} in={searchBarActive || searchActive}>
-                    <aside style={{transform: searchActive ? `translateY(-76px)` : undefined}}>
+                <Slide direction={"down"} in={searchBarActive || searchActive}>
+                    <aside
+                        style={{
+                            transform: searchActive
+                                ? `translateY(-76px)`
+                                : undefined
+                        }}
+                    >
                         <SearchBar />
                     </aside>
                 </Slide>
                 <Backdrop
-                    open={searchBarActive} 
-                    className="search-backdrop" 
+                    open={searchBarActive}
+                    className="search-backdrop"
                     onClick={this._closeSearchBar}
-                    style={{visibility: searchBarActive ? 'visible' : 'hidden'}}
+                    style={{
+                        visibility: searchBarActive ? "visible" : "hidden"
+                    }}
                 />
                 <VideoListing />
                 <footer>
@@ -64,4 +75,4 @@ class BrowseView extends Component {
     }
 }
 
-export default withSearch(BrowseView)
+export default withSearch(BrowseView);
