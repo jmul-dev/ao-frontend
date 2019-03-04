db6e287c5b496db183cf86adcffd01c46d00cab2
Neil Harlow <neil@y-designs.com>
2019-03-04 10:53:00 -0800

    small amount of styling to dappview (and succesfully render of dapp)

--- src/views/dapp/DappView.js
+++ src/views/dapp/DappView.js
@@ -7,6 +7,9 @@ import PropTypes from "prop-types";
 import { graphql } from "react-apollo";
 import { ContentFieldsWithFragments } from "../../graphql/fragments/ContentFields";
 import gql from "graphql-tag";
+import { withStyles } from "@material-ui/core/styles";
+import ButtonBase from "@material-ui/core/ButtonBase";
+import CloseIcon from "@material-ui/icons/Close";
 
 class DappView extends PureComponent {
     static propTypes = {
@@ -16,8 +19,9 @@ class DappView extends PureComponent {
                 contentId: PropTypes.string.isRequired
             })
         }),
+        history: PropTypes.any,
         // withDappContentQuery
-        data: PropTypes.shape({
+        userContentQuery: PropTypes.shape({
             loading: PropTypes.bool.isRequired,
             error: PropTypes.error,
             userContent: PropTypes.any
@@ -45,8 +49,8 @@ class DappView extends PureComponent {
     }
     componentDidUpdate(prevProps) {
         if (this.state.isElectron && !this.state.dappRendered) {
-            if (this.props.data.userContent) {
-                const dappUrl = this.props.data.userContent.fileUrl;
+            if (this.props.userContentQuery.userContent) {
+                const dappUrl = this.props.userContentQuery.userContent.fileUrl;
                 window.chrome.ipcRenderer.send("OPEN_DAPP_WINDOW", dappUrl);
                 this.setState({
                     dappRendered: true
@@ -54,20 +58,33 @@ class DappView extends PureComponent {
             }
         }
     }
+    _onClose = () => {
+        this.props.history.goBack();
+    };
     render() {
+        const { classes } = this.props;
         const { contentId } = this.props.match.params;
-        const { loading, error, userContent } = this.props.data;
+        const { loading, error, userContent } = this.props.userContentQuery;
         const { isElectron, dappRendered } = this.state;
         return (
             <View className={"DappView"} padding="none">
                 <section style={{ height: "100%", width: "100%" }}>
-                    <div className="dapp-status-bar">
+                    <div className={classes.statusBar}>
+                        <ButtonBase
+                            title={`Close Dapp`}
+                            className={classes.closeButton}
+                            onClick={this._onClose}
+                        >
+                            <CloseIcon />
+                        </ButtonBase>
                         Dapp status bar (verified, name, close, etc..)
                     </div>
-                    <div className="dapp-window-container">
+                    <div className={classes.dappWindowContainer}>
                         {!isElectron && userContent && (
                             <iframe
-                                src={userContent.fileUrl}
+                                src={`${process.env.REACT_APP_AO_CORE_URL}/${
+                                    userContent.fileUrl
+                                }`}
                                 style={{
                                     height: "100%",
                                     width: "100%",
@@ -81,6 +98,26 @@ class DappView extends PureComponent {
         );
     }
 }
+
+const styles = ({ palette }) => ({
+    statusBar: {
+        height: 40,
+        backgroundColor: palette.secondary.main,
+        color: "white",
+        display: "flex",
+        alignItems: "center"
+    },
+    closeButton: {
+        height: 40,
+        width: 40,
+        marginRight: 8
+    },
+    dappWindowContainer: {
+        height: `calc(100% - 40px)`,
+        overflow: "auto",
+        backgroundColor: "white"
+    }
+});
 
 const dappContentQuery = gql(`
     query userContent($id: ID!) {
@@ -103,6 +140,7 @@ const withDappContentQuery = graphql(dappContentQuery, {
 });
 
 export default compose(
+    withStyles(styles, { withTheme: true }),
     withEthAddress,
     withDappContentQuery
 )(DappView);
