27dd61c2d195260b70f6d7f79cb09be3cc14cb1c
Neil Harlow <neil@y-designs.com>
2019-04-16 13:42:25 -0700

    wrong logic for disabling debug log button

--- src/modules/settings/components/DebugLog.js
+++ src/modules/settings/components/DebugLog.js
@@ -1,11 +1,11 @@
 import React from "react";
 import DebugIcon from "@material-ui/icons/BugReport";
-import { IconButton, Button } from "@material-ui/core";
+import { IconButton } from "@material-ui/core";
 import { isElectron } from "../../../utils/electron";
 import { AO_CONSTANTS } from "ao-library";
 
 export default () => {
-    const disabled = isElectron();
+    const disabled = !isElectron();
     const onClick = () => {
         window.chrome.ipcRenderer.send(AO_CONSTANTS.IPC.OPEN_DEBUG_LOG);
     };
